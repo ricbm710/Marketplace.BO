b9e70e07dfd396f30229b2083d124b2e74b7abbe
Ricardo <ricardobarmont@gmail.com>
2024-12-09 11:28:05 -0400

    Added Layout file & Tailwind Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+//rd
 import { Route, Routes } from "react-router-dom";
+//Layout
+import Layout from "./Layout";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<div>Marketplace.BO</div>}></Route>
+        <Route path="/" element={<Layout />}></Route>
       </Routes>
     </>
   );
---
+++ src/components/main/Navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div>Navbar for MARKETPLACE.BO</div>;
+};
+
+export default Navbar;
